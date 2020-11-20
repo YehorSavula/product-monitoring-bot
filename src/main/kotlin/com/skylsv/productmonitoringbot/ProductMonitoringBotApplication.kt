168cdc094e79071ffbf1c12e4f24191f8477e15c
Yehor Savula <yehor-home@MBP-Yehor.maxnet.ua>
2020-11-20 18:49:04 +0200

    Initial commit

---
+++ src/main/kotlin/com/skylsv/productmonitoringbot/ProductMonitoringBotApplication.kt
@@ -0,0 +1,11 @@
+package com.skylsv.productmonitoringbot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProductMonitoringBotApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProductMonitoringBotApplication>(*args)
+}
---
+++ src/test/kotlin/com/skylsv/productmonitoringbot/ProductMonitoringBotApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.skylsv.productmonitoringbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProductMonitoringBotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
